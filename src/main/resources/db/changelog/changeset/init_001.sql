2fd6c3f6226da25e55bc348a4659a713a027d8ea
Suhrob <suhrob.a001@gmail.com>
2025-05-08 20:30:39 +0500

    security final

--- src/main/resources/db/changelog/changeset/init_001.sql
+++ src/main/resources/db/changelog/changeset/init_001.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
                        last_name VARCHAR(255),
                        username VARCHAR(255) NOT NULL UNIQUE,
                        email VARCHAR(255) NOT NULL UNIQUE,
-                       password VARCHAR(50) NOT NULL,
+                       password VARCHAR(255) NOT NULL,
                        is_active BOOLEAN DEFAULT TRUE NOT NULL,
                        is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
                        refresh_token VARCHAR(1000),
